84488ff1d66d337467b6f80269511719a705c998
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-08-24 00:47:45 +0200

    - small improvement

--- src/traact_gui/debug_run/ImageComponent.cpp
+++ src/traact_gui/debug_run/ImageComponent.cpp
@@ -30,6 +30,10 @@ void ImageComponent::update(buffer::ComponentBuffer &buffer, std::vector<RenderC
 void ImageComponent::uploadImage(buffer::ComponentBuffer &data) {
     SPDLOG_TRACE("{0}: uploadImage {1}", port_name_, data.getTimestamp());
 
+    if(!data.isInputValid(port_index_)){
+        return;
+    }
+
     const auto& image = data.getInput<vision::ImageHeader>(port_index_).value();
     const auto& image_header = data.getInputHeader<vision::ImageHeader>(port_index_);
 
@@ -41,32 +45,42 @@ void ImageComponent::uploadImage(buffer::ComponentBuffer &data) {
     ++upload_count_;
 
     auto data_size = image_header.width * image_header.height * image_header.channels * getBytes(image_header.base_type);
+    auto opengl_internal_format = getOpenGlInternalFormat(image_header);
+    auto opengl_format = getOpenGlFormat(image_header);
     if (!init_texture_) {
-        glGenTextures(1, &texture_);
-        glBindTexture(GL_TEXTURE_2D, texture_);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glBindTexture(GL_TEXTURE_2D, 0);
 
-        glGenBuffers(1, &pbo_id_);
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo_id_);
-        glBufferData(GL_PIXEL_UNPACK_BUFFER, data_size, 0, GL_STREAM_DRAW);
-        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 
+        checkOpenGLErrors(glGenTextures(1, &texture_));
+        checkOpenGLErrors(glBindTexture(GL_TEXTURE_2D, texture_));
+        checkOpenGLErrors(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+        checkOpenGLErrors(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+        //checkOpenGLErrors(glBindTexture(GL_TEXTURE_2D, 0));
+
+        checkOpenGLErrors(glGenBuffers(1, &pbo_id_));
+        checkOpenGLErrors(glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo_id_));
+        //checkOpenGLErrors(glBufferData(GL_PIXEL_UNPACK_BUFFER, data_size, 0, GL_STREAM_DRAW));
+        //checkOpenGLErrors(glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0));
+
+        SPDLOG_INFO("initialize with image component info: internal format {0} width {1} height {2} format {3}",opengl_internal_format, image_header.width, image_header.height, opengl_format);
 
         renderer_->setImageSize(ImVec2(image_header.width, image_header.height), window_name_);
 
         init_texture_ = true;
-    }
-    glBindTexture(GL_TEXTURE_2D, texture_);
-    //glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
-    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo_id_);
-    glBufferData(GL_PIXEL_UNPACK_BUFFER, data_size, image.data, GL_STREAM_DRAW);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_header.width, image_header.height, 0,
-                 getOpenGl(image_header.pixel_format), getOpenGl(image_header.base_type), 0);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    } else {
+        checkOpenGLErrors(glBindTexture(GL_TEXTURE_2D, texture_));
+        checkOpenGLErrors(glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo_id_));
 
-    glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+    }
+
+    checkOpenGLErrors(glBufferData(GL_PIXEL_UNPACK_BUFFER, data_size, image.data, GL_STREAM_DRAW));
+
+//    checkOpenGLErrors(glTexImage2D(GL_TEXTURE_2D, 0, opengl_internal_format, image_header.width, image_header.height, 0,
+//                                   opengl_format, GL_UNSIGNED_BYTE, 0));
+    checkOpenGLErrors(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_header.width, image_header.height, 0,
+                 getOpenGl(image_header.pixel_format), getOpenGl(image_header.base_type), 0));
+
+    checkOpenGLErrors(glBindTexture(GL_TEXTURE_2D, 0));
+    checkOpenGLErrors(glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0));
 }
 void ImageComponent::draw() {
     ImGui::Begin(window_name_.c_str(), nullptr, ImGuiWindowFlags_NoDocking);
